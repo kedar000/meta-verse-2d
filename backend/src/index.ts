f6ef797310a18f80f0065fd87371d7df9fb096ac
kedar000 <140879080+kedar000@users.noreply.github.com>
2025-06-24 20:23:02 +0530

    Modified ; schema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello form index.js ")
+// console.log("hello form index.js ")
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(` Server is running at http://localhost:${port}`);
+});
